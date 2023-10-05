28a80e7ee95606bd1bc1f8f9246ffaf163169abb
singhbergen <simerjit@dhammi.no>
2023-10-05 18:10:20 +0200

    Black background

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
+import { Stack } from "@mui/material";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <>
+  <Stack bgcolor={"#000000"}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <App />
-  </>
+  </Stack>
 );
